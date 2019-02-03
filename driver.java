d740f3bfc630f0fc302447b3df819e3c7c188c86
sxiao1 <sxiao1@stuy.edu>
2019-02-03 13:29:56 -0500

    finished recursion

---
+++ driver.java
@@ -0,0 +1,8 @@
+public class driver{
+  public static void main(String[]args){
+    System.out.println(recursion.sqrt(100, 0.001));
+    System.out.println(recursion.sqrt(1000, 0.001));
+    System.out.println(recursion.sqrt(500, 0.001));
+    System.out.println(recursion.sqrt(900, 0.001));
+  }
+}
--- recursion.java
+++ recursion.java
@@ -3,10 +3,15 @@ public class recursion{
     return sqrt2(n, 1, tolerance); //using helper function
   }
   public static double sqrt2(double n, double guess, double tolerance){
-    double error = tolerance * 0.01;
+    double error = tolerance / 0.01;
     if(n == 0){
       return 0;
     }
-    return n;
+    if((Math.abs((guess * guess) - n) / n) > tolerance){
+      return sqrt2(n, ( n / guess + guess) / 2, tolerance);
+    }
+    else{
+      return guess;
+    }
   }
 }
