d0a4ec2e7ba96447ebd95cb5d43ac9f19f19829f
sxiao1 <sxiao1@stuy.edu>
2019-02-25 10:05:50 -0500

    changed driver

--- driver.java
+++ driver.java
@@ -1,6 +1,6 @@
 public class driver{
   public static void main(String[]args){
-    System.out.println(recursion.sqrt(100, 0.001));
+    /*System.out.println(recursion.sqrt(100, 0.001));
     System.out.println(recursion.sqrt(1000, 0.001));
     System.out.println(recursion.sqrt(500, 0.001));
     System.out.println(recursion.sqrt(900, 0.001));
@@ -11,6 +11,8 @@ public class driver{
     System.out.println(recursion.makeAllSums(3));
     System.out.println(recursion.makeAllSums(0));
     System.out.println(recursion.makeAllSums(2));
-    System.out.println(recursion.makeAllSums(5));
+    System.out.println(recursion.makeAllSums(5)); */
+    System.out.println(recursion.checker(0));
   }
+
 }
